5e4b525ea158fb9e5a904446b8c669eeea801a5c
ennnnny <kuye1130@gmail.com>
2025-02-20 10:50:46 +0800

    release 1.0.3

--- src/Traits/UploadTrait.php
+++ src/Traits/UploadTrait.php
@@ -20,8 +20,8 @@ trait UploadTrait
     {
         $info = CloudResource::query()->find($resource_id, ['id', 'url']);
         if ($info) {
-            if (! empty($openDomain)) {
-                return $openDomain.$info->url;
+            if (!empty($openDomain)) {
+                return $openDomain . $info->url;
             }
             try {
                 $res = $info->getCloudStoragePath($info->url, $info->id);
@@ -41,13 +41,13 @@ trait UploadTrait
      *
      * @throws \Exception
      */
-    public function cloudSimpleUpload($file_path, $storage_id, $fileName = '')
+    public function cloudSimpleUpload($file_path, $storage_id, $fileName = '', $cover = false)
     {
         //file exist？
-        if (! file_exists($file_path)) {
+        if (!file_exists($file_path)) {
             return null;
         }
-        $storage = CloudStorage::query()->find($storage_id);
+        $storage = CloudStorage::query()->where('enabled', 1)->find($storage_id);
         if ($storage) {
             $service = new CloudUploadService;
             $size = filesize($file_path);
@@ -55,18 +55,21 @@ trait UploadTrait
             if (empty($fileName)) {
                 $file_res = getFilenameAndExtension($file_path);
                 $fileName = $file_res['filename'];
-                if (! empty($file_res['extension'])) {
-                    $fileName .= '.'.$file_res['extension'];
+                if (!empty($file_res['extension'])) {
+                    $fileName .= '.' . $file_res['extension'];
                 }
             }
             $object = $service->generateFileName($fileName, $config);
             $exits = CloudResource::query()->where('url', $object)->first();
-            if ($exits) {
+            if ($exits && !$cover) {
                 return $exits;
             }
             // 调用获取云存储服务
             $cloudStorageFactory = CloudStorageFactory::make($storage);
             $data = $cloudStorageFactory->receiver($object, $file_path);
+            if ($exits && $cover) {
+                return $exits;
+            }
             // 插入数据库
             $service->store(array_merge($data, ['size' => $size]), $storage_id);
 
@@ -81,30 +84,30 @@ trait UploadTrait
      *
      * @throws \Exception
      */
-    public function cloudChunkUpload($file_path, $storage_id, $fileName = '', $chunk_size = 50 * 1024 * 1024, $min_size = 5 * 1024 * 1024 * 1024)
+    public function cloudChunkUpload($file_path, $storage_id, $fileName = '', $chunk_size = 50 * 1024 * 1024, $min_size = 5 * 1024 * 1024 * 1024, $cover = false)
     {
         //file exist？
-        if (! file_exists($file_path)) {
+        if (!file_exists($file_path)) {
             return null;
         }
-        $storage = CloudStorage::query()->find($storage_id);
+        $storage = CloudStorage::query()->where('enabled', 1)->find($storage_id);
         if ($storage) {
             $size = filesize($file_path);
             if ($size < $min_size) {
-                return $this->cloudSimpleUpload($file_path, $storage_id, $fileName);
+                return $this->cloudSimpleUpload($file_path, $storage_id, $fileName, $cover);
             } else {
                 $service = new CloudUploadService;
                 $config = $storage->config;
                 if (empty($fileName)) {
                     $file_res = getFilenameAndExtension($file_path);
                     $fileName = $file_res['filename'];
-                    if (! empty($file_res['extension'])) {
-                        $fileName .= '.'.$file_res['extension'];
+                    if (!empty($file_res['extension'])) {
+                        $fileName .= '.' . $file_res['extension'];
                     }
                 }
                 $object = $service->generateFileName($fileName, $config);
                 $exits = CloudResource::query()->where('url', $object)->first();
-                if ($exits) {
+                if ($exits && !$cover) {
                     return $exits;
                 }
                 // 调用获取云存储服务
@@ -115,20 +118,20 @@ trait UploadTrait
                 $file = fopen($file_path, 'r');
                 $partNumber = 1;
                 $partList = [];
-                while (! feof($file)) {
+                while (!feof($file)) {
                     $part = fread($file, $chunk_size);
                     if ($part === false) {
                         break;
                     }
                     //$part临时储存
-                    if (Storage::disk('local')->put($uploadId.'_'.$partNumber, $part)) {
-                        $file_path_temp = Storage::disk('local')->path($uploadId.'_'.$partNumber);
+                    if (Storage::disk('local')->put($uploadId . '_' . $partNumber, $part)) {
+                        $file_path_temp = Storage::disk('local')->path($uploadId . '_' . $partNumber);
                         $data = $cloudStorageFactory->chunk($file_path_temp, $object, $uploadId, $partNumber, $chunk_size);
                         $partList[] = [
                             'partNumber' => $partNumber,
                             'eTag' => $data['eTag'],
                         ];
-                        Storage::disk('local')->delete($uploadId.'_'.$partNumber);
+                        Storage::disk('local')->delete($uploadId . '_' . $partNumber);
                         $partNumber++;
                     } else {
                         $partList = [];
@@ -137,9 +140,14 @@ trait UploadTrait
                 }
                 if (count($partList) > 0) {
                     $res = $cloudStorageFactory->finishChunk($object, $uploadId, $partList);
-                    $service->store(array_merge($res, ['size' => $size]));
+                    if ($exits && $cover) {
+                        return $exits;
+                    }
+                    $service->store(array_merge($res, ['size' => $size]), $storage_id);
 
                     return CloudResource::query()->where('url', $object)->first();
+                } elseif ($exits) {
+                    return $exits;
                 }
             }
         }
